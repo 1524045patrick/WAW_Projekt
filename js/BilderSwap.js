d086e871772de9ea8a4a17b588c6f36f4ad5d4c0
BuildTools <unconfigured@null.spigotmc.org>
2016-11-16 18:59:00 +0100

    index.html, Dropdown, Markus&Jens

---
+++ js/BilderSwap.js
@@ -0,0 +1,2 @@
+
+<script type="text/javascript" language="JavaScript"> <!-- // w. jansen web-toolbox.net function swap(Bildname,BildURL) { document.images[Bildname].src = BildURL; } //--> </script>
