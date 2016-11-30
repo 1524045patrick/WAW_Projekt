fd533d7d1d715d76cce6ede27f967040a25d92ec
BuildTools <unconfigured@null.spigotmc.org>
2016-11-30 17:12:23 +0100

    :lipstick:  :art:  index.html, styleIndex.css, ChangeBody.html created, js.js for Changing Bodys Markus&Jens

--- js/BilderSwap.js
+++
@@ -1,2 +0,0 @@
-
-<script type="text/javascript" language="JavaScript"> <!-- // w. jansen web-toolbox.net function swap(Bildname,BildURL) { document.images[Bildname].src = BildURL; } //--> </script>
