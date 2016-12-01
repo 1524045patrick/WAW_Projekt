60735d9598d207059183c5ab5e2a3bd67b0f596b
BuildTools <unconfigured@null.spigotmc.org>
2016-12-01 22:56:07 +0100

    :sparkles: :shit: :lipstick: ChangeBody.html, styleIndex.css, index.html,  created ChapterLinks Markus&Jens

--- js/js.js
+++ js/js.js
@@ -11,5 +11,69 @@ function passwortAendern(){
 function profilLöschen(){
     document.body.style.backgroundColor = "white";
     $('#content').load('ChangeBody.html #profilLöschen');
+}
 
+function chapter1(){
+    document.body.style.backgroundColor = "#a1c153";
+    $('#content').load('ChangeBody.html #chapter1');
+}
+function chapter2(){
+    document.body.style.backgroundColor = "#dbe283";
+    $('#content').load('ChangeBody.html #chapter2');
+}
+function chapter3(){
+    document.body.style.backgroundColor = "#bad151";
+    $('#content').load('ChangeBody.html #chapter3');
+}
+function chapter4(){
+    document.body.style.backgroundColor = "#7cc3a3";
+    $('#content').load('ChangeBody.html #chapter4');
+}
+function chapter5(){
+    document.body.style.backgroundColor = "#ffea64";
+    $('#content').load('ChangeBody.html #chapter5');
+}
+function chapter6(){
+    document.body.style.backgroundColor = "#fff3d8";
+    $('#content').load('ChangeBody.html #chapter6');
+}
+function chapter7(){
+    document.body.style.backgroundColor = "#ffcf53";
+    $('#content').load('ChangeBody.html #chapter7');
+}
+function chapter8(){
+    document.body.style.backgroundColor = "#f5a04c";
+    $('#content').load('ChangeBody.html #chapter8');
+}
+function chapter9(){
+    document.body.style.backgroundColor = "#e35184";
+    $('#content').load('ChangeBody.html #chapter9');
+}
+function chapter10(){
+    document.body.style.backgroundColor = "#ee7ba9";
+    $('#content').load('ChangeBody.html #chapter10');
+}
+function chapter11(){
+    document.body.style.backgroundColor = "#f7bed2";
+    $('#content').load('ChangeBody.html #chapter11');
+}
+function chapter12(){
+    document.body.style.backgroundColor = "#c9427e";
+    $('#content').load('ChangeBody.html #chapter12');
+}
+function chapter13(){
+    document.body.style.backgroundColor = "#4fa8da";
+    $('#content').load('ChangeBody.html #chapter13');
+}
+function chapter14(){
+    document.body.style.backgroundColor = "#94d3e5";
+    $('#content').load('ChangeBody.html #chapter14');
+}
+function chapter15(){
+    document.body.style.backgroundColor = "#005daa";
+    $('#content').load('ChangeBody.html #chapter15');
+}
+function chapter16(){
+    document.body.style.backgroundColor = "#658bc8";
+    $('#content').load('ChangeBody.html #chapter16');
 }
