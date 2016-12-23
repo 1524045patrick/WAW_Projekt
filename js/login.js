2ebfdf6f1cbf8fc00b5cf567f0d980555b72e9ee
JensWin <jenswindisch@gmx.de>
2016-12-23 17:14:16 +0100

    login.js, main.html: login geht über POST, Links im Hilfemenü eingefügt, Jens

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ function loginButton(){
     var tokenJSON = {
         "async": false,
         "url": "http://46.101.204.215:1337/api/V1/login",
-        "method": "PUT" }
+        "method": "POST" }
 
     //Befehl um token zu bekommen
     $.ajax(tokenJSON).done(function (response) {
